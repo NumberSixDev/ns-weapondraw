1c1caab13eadab3aef2e982de75145a53e1ba095
NumberSixDev <38400768+NumberSixDev@users.noreply.github.com>
2024-02-25 11:51:10 +0000

    Update weapdraw.lua

--- weapdraw.lua
+++ weapdraw.lua
@@ -1,122 +1,39 @@
--- weapdraw.lua
-
+-- Register Weapons
 local weapons = {
-	'WEAPON_KNIFE',
-    'WEAPON_SHIV',
-	'WEAPON_KATANA',
-	'WEAPON_NIGHTSTICK',
-	'WEAPON_BREAD',
-	'WEAPON_FLASHLIGHT',
-	'WEAPON_HAMMER',
-	'WEAPON_BAT',
-	'WEAPON_GOLFCLUB',
-	'WEAPON_CROWBAR',
-	'WEAPON_BOTTLE',
-	'WEAPON_DAGGER',
-	'WEAPON_HATCHET',
-	'WEAPON_MACHETE',
-	'WEAPON_SWITCHBLADE',
-	'WEAPON_BATTLEAXE',
-	'WEAPON_POOLCUE',
-	'WEAPON_WRENCH',
-	'WEAPON_PISTOL',
-	'WEAPON_PISTOL_MK2',
-	'WEAPON_COMBATPISTOL',
-	'WEAPON_APPISTOL',
-	'WEAPON_PISTOL50',
-	'WEAPON_REVOLVER',
-	'WEAPON_SNSPISTOL',
-	'WEAPON_HEAVYPISTOL',
-	'WEAPON_VINTAGEPISTOL',
-	'WEAPON_MICROSMG',
-	'WEAPON_SMG',
-	'WEAPON_ASSAULTSMG',
-	'WEAPON_MINISMG',
-	'WEAPON_MACHINEPISTOL',
-	'WEAPON_COMBATPDW',
-	'WEAPON_PUMPSHOTGUN',
-	'WEAPON_SAWNOFFSHOTGUN',
-	'WEAPON_ASSAULTSHOTGUN',
-	'WEAPON_BULLPUPSHOTGUN',
-	'WEAPON_HEAVYSHOTGUN',
-	'WEAPON_ASSAULTRIFLE',
-	'WEAPON_CARBINERIFLE',
-	'WEAPON_ADVANCEDRIFLE',
-	'WEAPON_SPECIALCARBINE',
-	'WEAPON_BULLPUPRIFLE',
-	'WEAPON_COMPACTRIFLE',
-	'WEAPON_MG',
-	'WEAPON_COMBATMG',
-	'WEAPON_GUSENBERG',
-	'WEAPON_SNIPERRIFLE',
-	'WEAPON_HEAVYSNIPER',
-	'WEAPON_MARKSMANRIFLE',
-	'WEAPON_GRENADELAUNCHER',
-	'WEAPON_RPG',
-	'WEAPON_STINGER',
-	'WEAPON_MINIGUN',
-	'WEAPON_GRENADE',
-	'WEAPON_STICKYBOMB',
-	'WEAPON_SMOKEGRENADE',
-	'WEAPON_BZGAS',
-	'WEAPON_MOLOTOV',
-	'WEAPON_DIGISCANNER',
-	'WEAPON_FIREWORK',
-	'WEAPON_MUSKET',
-	'WEAPON_STUNGUN',
-	'WEAPON_HOMINGLAUNCHER',
-	'WEAPON_PROXMINE',
-	'WEAPON_FLAREGUN',
-	'WEAPON_MARKSMANPISTOL',
-	'WEAPON_RAILGUN',
-	'WEAPON_DBSHOTGUN',
-	'WEAPON_AUTOSHOTGUN',
-	'WEAPON_COMPACTLAUNCHER',
-	'WEAPON_PIPEBOMB',
-	'WEAPON_DOUBLEACTION',
-	'WEAPON_PEPPERSPRAY',
-	'WEAPON_ANTIDOTE',
-	'WEAPON_COMBATSHOTGUN',
-	'WEAPON_CERAMICPISTOL',
-    'WEAPON_FNX45',
-	'WEAPON_HK416',
-	'WEAPON_MAC10',
-	'WEAPON_UZI',
-	'WEAPON_SLEDGEHAMMER',
-	'WEAPON_KARAMBIT',
-    'WEAPON_BRICK',
-    'WEAPON_BRICK2',
-    'WEAPON_MP5'
+    'WEAPON_KNIFE', 'WEAPON_SHIV', 'WEAPON_KATANA', 'WEAPON_NIGHTSTICK', 'WEAPON_BREAD',
+    'WEAPON_FLASHLIGHT', 'WEAPON_HAMMER', 'WEAPON_BAT', 'WEAPON_GOLFCLUB', 'WEAPON_CROWBAR',
+    'WEAPON_BOTTLE', 'WEAPON_DAGGER', 'WEAPON_HATCHET', 'WEAPON_MACHETE', 'WEAPON_SWITCHBLADE',
+    'WEAPON_BATTLEAXE', 'WEAPON_POOLCUE', 'WEAPON_WRENCH', 'WEAPON_PISTOL', 'WEAPON_PISTOL_MK2',
+    'WEAPON_COMBATPISTOL', 'WEAPON_APPISTOL', 'WEAPON_PISTOL50', 'WEAPON_REVOLVER', 'WEAPON_SNSPISTOL',
+    'WEAPON_HEAVYPISTOL', 'WEAPON_VINTAGEPISTOL', 'WEAPON_MICROSMG', 'WEAPON_SMG', 'WEAPON_ASSAULTSMG',
+    'WEAPON_MINISMG', 'WEAPON_MACHINEPISTOL', 'WEAPON_COMBATPDW', 'WEAPON_PUMPSHOTGUN', 'WEAPON_SAWNOFFSHOTGUN',
+    'WEAPON_ASSAULTSHOTGUN', 'WEAPON_BULLPUPSHOTGUN', 'WEAPON_HEAVYSHOTGUN', 'WEAPON_ASSAULTRIFLE',
+    'WEAPON_CARBINERIFLE', 'WEAPON_ADVANCEDRIFLE', 'WEAPON_SPECIALCARBINE', 'WEAPON_BULLPUPRIFLE',
+    'WEAPON_COMPACTRIFLE', 'WEAPON_MG', 'WEAPON_COMBATMG', 'WEAPON_GUSENBERG', 'WEAPON_SNIPERRIFLE',
+    'WEAPON_HEAVYSNIPER', 'WEAPON_MARKSMANRIFLE', 'WEAPON_GRENADELAUNCHER', 'WEAPON_RPG', 'WEAPON_STINGER',
+    'WEAPON_MINIGUN', 'WEAPON_GRENADE', 'WEAPON_STICKYBOMB', 'WEAPON_SMOKEGRENADE', 'WEAPON_BZGAS',
+    'WEAPON_MOLOTOV', 'WEAPON_DIGISCANNER', 'WEAPON_FIREWORK', 'WEAPON_MUSKET', 'WEAPON_STUNGUN',
+    'WEAPON_HOMINGLAUNCHER', 'WEAPON_PROXMINE', 'WEAPON_FLAREGUN', 'WEAPON_MARKSMANPISTOL', 'WEAPON_RAILGUN',
+    'WEAPON_DBSHOTGUN', 'WEAPON_AUTOSHOTGUN', 'WEAPON_COMPACTLAUNCHER', 'WEAPON_PIPEBOMB', 'WEAPON_DOUBLEACTION',
+    'WEAPON_COMBATSHOTGUN', 'WEAPON_CERAMICPISTOL'
 }
 
+-- Police Weapons For Holster Anim
 local holsterableWeapons = {
-	'WEAPON_STUNGUN',
-	'WEAPON_PISTOL',
-	'WEAPON_COMBATPISTOL',
-	'WEAPON_NIGHTSTICK',
-	'WEAPON_FLASHLIGHT',
-	'WEAPON_MICROSMG',
-	'WEAPON_SPECIALCARBINE',
-    'WEAPON_MP5',
-    'WEAPON_HK416',
-}
-
-local damageModifiers = {
-	[`WEAPON_SNOWBALL`] = 0.0,
-	[`WEAPON_PUMPSHOTGUN`] = 0.0
+    'WEAPON_STUNGUN', 'WEAPON_COMBATPISTOL', 'WEAPON_NIGHTSTICK', 'WEAPON_FLASHLIGHT'
 }
 
 local holstered = true
 local canFire = true
 local currWeapon = GetHashKey('WEAPON_UNARMED')
-local currentHoldster = nil
+local currentHolster = nil
 
 RegisterNetEvent('weapons:ResetHolster')
 AddEventHandler('weapons:ResetHolster', function()
-	holstered = true
-	canFire = true
-	currWeapon = GetHashKey('WEAPON_UNARMED')
-	currentHoldster = nil
+    holstered = true
+    canFire = true
+    currWeapon = GetHashKey('WEAPON_UNARMED')
+    currentHolster = nil
 end)
 
 CreateThread(function()
@@ -134,15 +51,10 @@ CreateThread(function()
 				loadAnimDict("rcmjosh4")
 				loadAnimDict("weapons@pistol@")
 
-				if damageModifiers[newWeap] ~= nil then
-					SetPlayerWeaponDamageModifier(PlayerId(), damageModifiers[newWeap])
-				else
-					SetPlayerWeaponDamageModifier(PlayerId(), 1.0)
-				end
-
 				if CheckWeapon(newWeap) then
 					if holstered then
-						if QBCore.Functions.GetPlayerData().job.name == "police" or QBCore.Functions.GetPlayerData().job.name == "security" or QBCore.Functions.GetPlayerData().job.name == "ambulance" then
+						job = QBCore.Functions.GetPlayerData().job.name
+						if job == "police" or job == "security" or job == "ambulance" then
 							canFire = false
 							currentHoldster = GetPedDrawableVariation(ped, 7)
 							TaskPlayAnimAdvanced(ped, "rcmjosh4", "josh_leadout_cop2", GetEntityCoords(ped, true), 0, 0, rot, 3.0, 3.0, -1, 50, 0, 0, 0)
@@ -165,7 +77,7 @@ CreateThread(function()
 							canFire = true
 						end
 					elseif newWeap ~= currWeapon and CheckWeapon(currWeapon) then
-						if QBCore.Functions.GetPlayerData().job.name == "police" or QBCore.Functions.GetPlayerData().job.name == "security" or QBCore.Functions.GetPlayerData().job.name == "ambulance" then
+						if job == "police" or job == "security" or job == "ambulance" then
 							canFire = false
 							TaskPlayAnimAdvanced(ped, "reaction@intimidation@cop@unarmed", "intro", GetEntityCoords(ped, true), 0, 0, rot, 3.0, 3.0, -1, 50, 0, 0, 0)
 							Wait(500)
@@ -194,7 +106,7 @@ CreateThread(function()
 							canFire = true
 						end
 					else
-						if QBCore.Functions.GetPlayerData().job.name == "police" or QBCore.Functions.GetPlayerData().job.name == "security" or QBCore.Functions.GetPlayerData().job.name == "ambulance" then
+						if job == "police" or job == "security" or job == "ambulance" then
 							SetCurrentPedWeapon(ped, GetHashKey('WEAPON_UNARMED'), true)
 							currentHoldster = GetPedDrawableVariation(ped, 7)
 							TaskPlayAnimAdvanced(ped, "rcmjosh4", "josh_leadout_cop2", GetEntityCoords(ped, true), 0, 0, rot, 3.0, 3.0, -1, 50, 0, 0, 0)
@@ -219,7 +131,7 @@ CreateThread(function()
 					end
 				else
 					if not holstered and CheckWeapon(currWeapon) then
-						if QBCore.Functions.GetPlayerData().job.name == "police" or QBCore.Functions.GetPlayerData().job.name == "security" or QBCore.Functions.GetPlayerData().job.name == "ambulance" then
+						if job == "police" or job == "security" or job == "ambulance" then
 							canFire = false
 							TaskPlayAnimAdvanced(ped, "reaction@intimidation@cop@unarmed", "intro", GetEntityCoords(ped, true), 0, 0, rot, 3.0, 3.0, -1, 50, 0, 0, 0)
 							Wait(500)
@@ -258,17 +170,21 @@ end)
 
 
 CreateThread(function()
-	while true do
-		if not canFire then
-			DisableControlAction(0, 25, true)
-			DisablePlayerFiring(PlayerPedId(), true)
-		else
-			Wait(250)
-		end
-
-		Wait(3)
-	end
+    while true do
+        if not canFire then
+            DisableControlAction(0, 25, true)
+            DisablePlayerFiring(PlayerPedId(), true)
+        end
+        Wait(100)
+    end
 end)
+
+function loadAnimDict(dict)
+	while (not HasAnimDictLoaded(dict)) do
+		RequestAnimDict(dict)
+		Wait(5)
+	end
+end
 
 function CheckWeapon(newWeap)
 	for i = 1, #weapons do
@@ -286,11 +202,4 @@ function IsWeaponHolsterable(weap)
 		end
 	end
 	return false
-end
-
-function loadAnimDict(dict)
-	while (not HasAnimDictLoaded(dict)) do
-		RequestAnimDict(dict)
-		Wait(5)
-	end
 end
